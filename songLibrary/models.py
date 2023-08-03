4a188722b8302dc8b6b71de2eca7a2f4644d803f
claytoncritchfield <clayton.critchfield@gmail.com>
2023-08-03 16:43:18 +0000

    imported decorators and Response in the app level views file

--- songLibrary/models.py
+++ songLibrary/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Song(models.Model):
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+    
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+class user(models.Model):
+    username = models.CharField(max_length=16)
+    email = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
+    
--- songLibrary/views.py
+++ songLibrary/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 # Create your views here.
 def index(request):
     return HttpResponse('Hi')
