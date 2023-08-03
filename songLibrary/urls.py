dd0f4dc9db44c2d2f1095e2322893010a39e5a3f
claytoncritchfield <clayton.critchfield@gmail.com>
2023-08-03 17:14:46 +0000

    added first functionality under the URL: song-list where a user can see ALL the songs in the databas e

--- songLibrary/urls.py
+++ songLibrary/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.apiOverview, name = 'index'),
+    path('song-list/', views.songList, name = 'song-list'),
 ]
--- songLibrary/views.py
+++ songLibrary/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import SongSerializer
 
-# Create your views here.
+from .models import Song
+# This view below makes it so when the user loads the front page of the API, it shows them all the possible URL paths
 @api_view(['GET'])
 def apiOverview(request):
     api_urls = {
@@ -16,3 +18,9 @@ def apiOverview(request):
     }
 
     return Response(api_urls)
+
+@api_view(['GET'])
+def songList(request):
+    songs = Song.objects.all()
+    serializer = SongSerializer(songs, many=True)
+    return Response(serializer.data)
